be2f419fbcc4d5b9a3409ba0b1b6639815be6ac7
Vinicius Bernardes Silva <vinicius.bsilva@hotmail.com>
2022-06-08 17:44:14 -0300

    end of example

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ app.get('/', (req, res) => {
     res.send('Hello from App Engine!');
 });
 
-// Listen to the App Engine-specified port, or 8080 otherwise
-const PORT = process.env.PORT || 8080;
+// Listen to the App Engine-specified port, or 9999 otherwise
+const PORT = process.env.PORT || 9999;
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}...`);
 });
